88363caeddf098abfaeb388cea7bb23ca8406abf
Enzo Haussecker <enzo@sovereign.io>
2018-04-18 21:19:22 -0700

    Fix up build script

--- default.nix
+++ default.nix
@@ -1,8 +1,20 @@
 { pkgs ? import <nixpkgs> {} }:
-    with pkgs;
-    let crypto = haskellPackages.callCabal2nix "dfinity-crypto" ./. {};
-    if pkgs.lib.inNixShell 
-    then stdenv.lib.overrideDerivation crypto.env (oldAttrs: {
-        nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ cabal-install stack ];
-    })
-    else crypto;
+  with pkgs;
+  let
+    math = import ../bn/bn.nix
+    {
+      inherit clang gnumake gmpxx llvm openssl;
+      stdenv = clangStdenv;
+    };
+    crypto = haskellPackages.callCabal2nix "dfinity-crypto" ./. {
+      bls384 = math;
+      mcl = math;
+    };
+  in
+  if pkgs.lib.inNixShell 
+  then stdenv.lib.overrideDerivation crypto.env (
+    oldAttrs: {
+      nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ cabal-install stack ];
+    }
+  )
+  else crypto
